a24c4cff1a2d4330125c3fe55742680698656b48
juliac-7032 <juliacawkwell3@gmail.com>
2024-03-16 12:50:17 -0600

    test for variable load

--- panel-tester-firmware/src/main.cpp
+++ panel-tester-firmware/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <INA219.h>
 
-// put function declarations here:
-int myFunction(int, int);
+INA219 INA(0x40);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT); //led
+  pinMode(10, OUTPUT);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, LOW); // for capacitor
+  Serial.begin(115200); //baud rate 
+  Serial.println("Setup complete");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on 
+
+
+  //analog write - 100 = 0.5V
+  analogWrite(10, 50);
+
+  //print information
+  float bus_voltage = INA.getBusVoltage_mV();
+  //float shunt_voltage = INA.getShuntVoltage_mV();
+  float current = INA.getCurrent_mA();
+
+  //Serial.println("Format: bus voltage (V), current(mA)");
+  Serial.print(bus_voltage, 2);
+  Serial.print(",");
+  Serial.print(current, 2);
+  Serial.println(",");
+
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off 
+  delay(1000);
+
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
