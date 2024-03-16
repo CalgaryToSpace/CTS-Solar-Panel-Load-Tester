051199c4d601ea79975bdc0d9a79677aadc9d595
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-03-16 16:06:04 -0600

    Convert to tabs

--- panel-tester-firmware/src/main.cpp
+++ panel-tester-firmware/src/main.cpp
@@ -4,37 +4,35 @@
 INA219 INA(0x40);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); //led
-  pinMode(10, OUTPUT);
-  pinMode(8, OUTPUT);
-  digitalWrite(8, LOW); // for capacitor
-  Serial.begin(115200); //baud rate 
-  Serial.println("Setup complete");
+	pinMode(LED_BUILTIN, OUTPUT); //led
+	pinMode(10, OUTPUT);
+	pinMode(8, OUTPUT);
+	digitalWrite(8, LOW); // for capacitor
+	Serial.begin(115200); //baud rate 
+	Serial.println("Setup complete");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on 
+	digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on 
 
 
-  //analog write - 100 = 0.5V
-  analogWrite(10, 50);
+	//analog write - 100 = 0.5V
+	analogWrite(10, 50);
 
-  //print information
-  float bus_voltage = INA.getBusVoltage_mV();
-  //float shunt_voltage = INA.getShuntVoltage_mV();
-  float current = INA.getCurrent_mA();
+	//print information
+	float bus_voltage = INA.getBusVoltage_mV();
+	//float shunt_voltage = INA.getShuntVoltage_mV();
+	float current = INA.getCurrent_mA();
 
-  //Serial.println("Format: bus voltage (V), current(mA)");
-  Serial.print(bus_voltage, 2);
-  Serial.print(",");
-  Serial.print(current, 2);
-  Serial.println(",");
+	//Serial.println("Format: bus voltage (V), current(mA)");
+	Serial.print(bus_voltage, 2);
+	Serial.print(",");
+	Serial.print(current, 2);
+	Serial.println(",");
 
-  delay(1000);                      
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off 
-  delay(1000);
+	delay(1000);                      
+	digitalWrite(LED_BUILTIN, LOW);   // turn the LED off 
+	delay(1000);
 
-  
+	
 }
-
-
