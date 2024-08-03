4ffe49f8f0b51f0d8d60f34bf749096d4b5c3cd9
zzaid17 <91937100+zzaid17@users.noreply.github.com>
2024-08-03 09:04:40 -0600

    Labeled HW SPI MCP call and .begin method. Imports SPI.h instead of Wire.h

--- panel-tester-firmware/src/main.cpp
+++ panel-tester-firmware/src/main.cpp
@@ -1,18 +1,18 @@
 #include "MCP_DAC.h"
-#include "Wire.h"
+#include "SPI.h"
 
 MCP4921 MCP(11, 13); // Args: MOSI=D11, CLK=D13
-// MCP4921 MCP;  //  HW SPI
+// MCP4921 MCP;  //  HW SPI, Default: MOSI=D11, CLK=D13
 
 volatile int x;
 uint32_t start, stop;
 
 float analog_to_voltage();
 
-void setup() {
+void setup() {  
   Serial.begin(115200);
   Serial.println(__FILE__);
-  SPI.begin();
+  // SPI.begin(); // Use with HW SPI only
 
   MCP.begin(10); // Set pin 10 to OUTPUT and HIGH
   
@@ -27,12 +27,19 @@ void setup() {
 void loop() {
   // DAC only has one channel (1), loop function loops through different values (0-4095)
   for (int value = 0; value <= 4096; value += 512) {
-    MCP.write(value, 1); // Args: value (0-4095), channel (1)
-    Serial.print("Analog read @ "); Serial.print(value); Serial.print(": "); 
-    Serial.print(analog_to_voltage()); Serial.println(" V");
+   // MCP.write(value, 20); // Args: value (0-4095), channel (1)
+    
+    if (MCP.write(value, 0) == false) {
+      Serial.print("invalid channel");
+    }
+
+   // Serial.print("Analog read @ "); Serial.print(value); Serial.print(": "); 
+   // Serial.print(analog_to_voltage()); Serial.println(" V");
+   Serial.print("Set");
+  delay(2000);
   }
 
-  delay(10000);
+  
 }
 
 float analog_to_voltage() {
