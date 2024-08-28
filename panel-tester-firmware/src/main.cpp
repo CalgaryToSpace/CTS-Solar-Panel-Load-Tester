e4dd9366c49d48face4a00aae326da0569704fc8
zzaid17 <91937100+zzaid17@users.noreply.github.com>
2024-08-27 19:48:28 -0600

    cleaned up unused variables, added note on output voltage scaling factor

--- panel-tester-firmware/src/main.cpp
+++ panel-tester-firmware/src/main.cpp
@@ -4,9 +4,6 @@
 MCP4921 MCP(11, 13); // Args: MOSI=D11, CLK=D13
 // MCP4921 MCP;  //  HW SPI, Default: MOSI=D11, CLK=D13
 
-volatile int x;
-uint32_t start, stop;
-
 float analog_to_voltage();
 
 void setup() {  
@@ -24,24 +21,29 @@ void setup() {
   Serial.println("\nLooping:");
 }
 
-void loop() {
-  // DAC only has one channel (1), loop function loops through different values (0-4095)
+void loop() { 
+  // DAC only has one channel (1), loop function loops through different values (0-4095)    
   for (int value = 0; value <= 4096; value += 512) {
-   // MCP.write(value, 20); // Args: value (0-4095), channel (1)
+    MCP.write(value, 1); // Args: value (0-4095), channel (1)
+    // Note that 4096 exceeds the max value of 4095 but for the purpose of including 4095 this is fine
     
     if (MCP.write(value, 0) == false) {
       Serial.print("invalid channel");
     }
+    
+    delay(2000);
 
-   // Serial.print("Analog read @ "); Serial.print(value); Serial.print(": "); 
-   // Serial.print(analog_to_voltage()); Serial.println(" V");
-   Serial.print("Set");
-  delay(2000);
+    Serial.print("Analog read @ "); Serial.print(value); Serial.print(": ");
+    Serial.print(analog_to_voltage()); Serial.println(" V");
+
+     delay(2000);
   }
-
-  
 }
 
 float analog_to_voltage() {
-  return 3.3 * analogRead(A1) / 1024; // Converts analog reading to voltage measurement
+  float Vout = 3.3 * ((float)analogRead(A1)) / 1024.0; 
+  // Output voltage formula (1024 = 2^10 for 10 bit DAC, Vref = 3.3 V)
+  return Vout;
+  // Output voltage is off by a scaling factor of ~1.33
+  // https://docs.google.com/spreadsheets/d/1Ryu8QGifAIBskT1q3D5ZtqSPAT-eqhYYwvQuIu554fw/edit?usp=sharing
 }
