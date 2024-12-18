55e1b34c3144dec4c8063b609051915009d22b8d
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-12-17 21:22:04 -0700

    Firmware: support PCB Rev 2

--- panel-tester-firmware/src/main.cpp
+++ panel-tester-firmware/src/main.cpp
@@ -1,145 +1,128 @@
-#include <Arduino.h>
-#include <INA219.h>
+#include "SPI.h"
+#include "MCP_DAC.h"
+#include "INA219.h"
 
-#include <Wire.h>
+// Chip select pin for MCP4921
+const uint8_t PIN_MCP_CS = 10;
+const uint8_t PIN_MCP_MOSI = 3;
+const uint8_t PIN_MCP_CLK = 13;
 
-// R100 shunt resistor = 0.1 ohms
-INA219 ina_dev(0x40);
+// Initialize DAC: MOSI=D3, CLK=D13.
+// Important note: If not using the default hardware pins (MOSI=D11, CLK=D13),
+// then DO NOT call `SPI.begin()`!
+MCP4921 dac(PIN_MCP_MOSI, PIN_MCP_CLK);
 
-// R2R LSB on D11
-// R2R MSB on D3 (9 bits) [set to 0 to keep low always, and make it an 8 bit DAC]
+// Initialize INA219 sensor at I2C address 0x40.
+INA219 current_sensor(0x40);
 
-void set_r2r_dac_value(uint8_t value) {
-  	digitalWrite(3, LOW); // always low
-
-	// bit_place = 0 is the LSB (rightmost)
-	for (uint8_t bit_place = 0; bit_place < 8; bit_place++) {
-		uint8_t bit = (value >> bit_place) & 0x01;
-		uint8_t pin = 11 - bit_place;
-		digitalWrite(pin, bit);
-	}
+uint16_t read_alt_current_sense_value()
+{
+    return analogRead(A0);
 }
 
-void setup() {
-	pinMode(LED_BUILTIN, OUTPUT); //led
-	
-	for (uint8_t pin = 3; pin <= 11; pin++) {
-		pinMode(pin, OUTPUT);
-	}
-
-	set_r2r_dac_value(0); // 0 means off, and thus no current flowing
-	
-	Serial.begin(115200); // baud rate
-	Serial.println("Serial started.");
-
-	// start I2C
-	Wire.begin();
-
-	// start INA219
-	ina_dev.begin();
-	ina_dev.setMaxCurrentShunt(1, 0.1); // 1A max, 0.1 ohm shunt resistor
-
-	Serial.print("INA219 Shunt Resistor (ohms): ");
-	Serial.println(ina_dev.getShunt());
-
-
-	Serial.println("setup() complete");
+/// @brief Reads the analog value from the solar panel and converts it to a voltage.
+/// @return The voltage of the solar panel, after applying voltage divider logic.
+int32_t read_solar_panel_voltage_with_on_chip_adc_mV()
+{
+    // Voltage divider: 10k on top, 2k on bottom.
+    const float v_sense = 3.3 * ((float)analogRead(A1)) / 1023.0;
+    const float v_panel = v_sense * (10.0 + 2.0) / 2.0;
+    return v_panel * 1000.0;
 }
 
-void debug_cycle_through_r2r_values() {
-	Serial.println("\nStarting debug_cycle_through_r2r_values()\n");
-
-	// set the DAC value to each value
-	for (uint8_t value = 0; value <= 255; value++) {
-		set_r2r_dac_value(value);
-		delay(50); // 50*250ms = 12.5 seconds
-		Serial.print(value);
-		Serial.print(" ");
-	}
-	Serial.println("\nDone debug_cycle_through_r2r_values()\n");
+/// @brief Sets the DAC value to a new value.
+/// @param new_value 12-bit value to set the DAC to (0 to 4095).
+void set_dac_value(uint16_t new_value)
+{
+    if (! dac.write(new_value, 0))
+    {
+        Serial.println("Failed to write to DAC.");
+    }
 }
 
-void test_current_at_each_r2r_value(uint8_t start_r2r_value, uint8_t max_r2r_value, uint8_t increment_amount) {
-	Serial.println("\nStarting test_current_at_each_r2r_value()\n");
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Booting...");
 
-	// print the CSV header
-	Serial.println("r2r_value,bus_voltage_V,current_mA,shunt_voltage_mV,shunt_adc");
+    // Init pin modes.
+    pinMode(PIN_MCP_CS, OUTPUT);
+    digitalWrite(PIN_MCP_CS, HIGH); // Disable chip select.
 
-	// set the DAC value to each value
-	for (uint16_t value = start_r2r_value; value <= max_r2r_value; value += increment_amount) {
-		set_r2r_dac_value(value);
-		delay(50); // wait to stabilize
+    pinMode(PIN_MCP_CLK, OUTPUT);
+    pinMode(PIN_MCP_MOSI, OUTPUT);
 
-		// print information
-		for (uint16_t i = 0; i < 10; i++) {
-			float bus_voltage_V = ina_dev.getBusVoltage();
-			float current_mA = ina_dev.getCurrent_mA();
-			float shunt_voltage_mV = ina_dev.getShuntVoltage_mV();
-			uint8_t shunt_adc = ina_dev.getShuntADC();
-			Serial.print(value);
-			Serial.print(",");
-			Serial.print(bus_voltage_V, 3);
-			Serial.print(",");
-			Serial.print(current_mA, 2);
-			Serial.print(",");
-			Serial.print(shunt_voltage_mV, 4);
-			Serial.print(",");
-			Serial.print(shunt_adc);
-			Serial.println();
-			delay(100); // TODO: change to 100 probably
-		}
-	}
-	Serial.println("\nDone test_current_at_each_r2r_value()\n");
+    dac.begin(PIN_MCP_CS); // Set chip select pin.
+    // Note: DO NOT call SPI.begin() if using software SPI (like we are).
+    Serial.println("MCP_DAC Initialized");
+
+    Wire.begin(); // Begin I2C communication (for INA219 sensor).
+    if (current_sensor.begin())
+    {
+        Serial.println("INA219 Sensor Connected");
+    }
+    else
+    {
+        Serial.println("INA219 sensor not connected.");
+    }
+
+    Serial.print("MCP_DAC_LIB_VERSION: ");
+    Serial.println(MCP_DAC_LIB_VERSION);
+    Serial.println();
+    Serial.print("CHANNELS: ");
+    Serial.println(dac.channels()); // Prints # of DAC channels (1)
+    Serial.print("MAXVALUE: ");
+    Serial.println(dac.maxValue()); // Prints max value (4095 for a 12 bit DAC)
+    Serial.print("INA219_LIB_VERSION: ");
+    Serial.println(INA219_LIB_VERSION);
+    Serial.println();
+
+    delay(1000);
+
+    // Configure current sensor with max current and shunt resistance.
+    current_sensor.setMaxCurrentShunt(1, 0.04);
+
+    delay(500); // Wait, in case INA current_sensor is calibrating.
+
+    Serial.println("End of setup()...");
 }
 
 
-void i2c_scanner() {
-	byte error, address;
-	int nDevices;
+void loop()
+{
+    for (uint16_t dac_value = 0; dac_value < 4096; dac_value += 50)
+    {
+        set_dac_value(dac_value);
 
-	Serial.println("Scanning...");
+        for (uint16_t log_num = 0; log_num < 10; log_num++)
+        {
+            char json_buffer[400];
 
-	nDevices = 0;
-	for(address = 1; address < 127; address++ )
-	{
-	// The i2c_scanner uses the return value of
-	// the Write.endTransmission to see if
-	// a device did acknowledge to the address.
-	Wire.beginTransmission(address);
-	error = Wire.endTransmission();
+            const int32_t adc_panel_voltage_mV = read_solar_panel_voltage_with_on_chip_adc_mV();
+            const int16_t alt_current_sense_value = read_alt_current_sense_value();
+            const int32_t bus_voltage_mV = current_sensor.getBusVoltage_mV();
+            const int32_t current_mA = current_sensor.getCurrent_mA();
 
-	if (error == 0)
-	{
-		Serial.print("I2C device found at address 0x");
-		if (address<16)
-		Serial.print("0");
-		Serial.print(address,HEX);
-		Serial.println("  !");
+            snprintf(
+                json_buffer, sizeof(json_buffer),
+                "{"
+                "\"uptime_ms\":%li,"
+                "\"bus_voltage_mV\":%li,"
+                "\"current_mA\":%li,"
+                "\"dac_value\":%" PRIi16 ","
+                "\"adc_panel_voltage_mV\":%li,"
+                "\"alt_current_sense\":%u"
+                "}",
+                millis(),
+                bus_voltage_mV,
+                current_mA,
+                dac_value,
+                adc_panel_voltage_mV,
+                alt_current_sense_value);
 
-		nDevices++;
-	}
-	else if (error==4)
-	{
-		Serial.print("Unknown error at address 0x");
-		if (address<16)
-		Serial.print("0");
-		Serial.println(address,HEX);
-	}    
-	}
-	if (nDevices == 0)
-	Serial.println("No I2C devices found\n");
-	else
-	Serial.println("done\n");
+            Serial.println(json_buffer);
 
-	delay(5000);           // wait 5 seconds for next scan
-}
-
-
-void loop() {
-	const uint8_t start_r2r_value = 200;
-	const uint8_t max_r2r_value = 250;
-	const uint8_t increment_amount = 1;
-	test_current_at_each_r2r_value(start_r2r_value, max_r2r_value, increment_amount);
-
-	// i2c_scanner();
+            delay(100);
+        }
+    }
 }
